f3a230cd54042b74887c1076fa7b146e72692f9d
Brad Bryant <bradleybryant86@gmail.com>
2019-12-13 20:02:06 -0600

    Added html and JS pages to the folder

---
+++ Password.js
@@ -0,0 +1,86 @@
+function getPasswordOptions() {
+    var length = parseInt(prompt("Specify password length. 8-128 chracters"));
+    if (isNan(length) === true) {
+        alert("Password length must be specified as a number");
+        
+    }
+    if (length < 8) {
+        alert("Password length must be at least 8 characters");
+        
+    }
+    if (length > 128) {
+        alert("Password length cannot be greater than 128 characters");
+        
+    }
+    var specChars = confirm("Click ok if you want to include special characters");
+    var numbers = confirm("Click ok to include numbers");
+    var upperCase = confirm("Click ok to include uppercase letters");
+    var lowerCase = confirm("Click ok to include lowercase letters");
+    if (
+        specChars === false &&
+        numbers === false &&
+        upperCase === false &&
+        lowerCase === false
+    ) {
+        alert("Select at least one character type")
+        
+    }
+}
+ getPasswordOptions();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var passLength =
+//     prompt("Specify password length: 8-128 characters.");
+// if (passLength > 8 || passLength > 128) {
+//     passLength = prompt("Invalid password length: 8-128 characters.");
+// }
+
+// if (passLength > 8 || passlength > 128) {
+//     location.reload();
+// }
+
+// else var specChars = confirm("Do you want to include special characters?")
+
+// var numbers = confirm("Do you want to include numbers?")
+
+// var upperCase = confirm("Do you want to include capital letters?")
+
+// var lowerCase = confirm("Do you want to include lower case letters?")
+
+// function generatePassword() {
+
+//     var charList = "";
+
+//     if (specChars) {
+//         charList = charList + "1234567890";
+//     }
+
+//     if (upperCase) {
+//         charList = charList + "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+//     }
+
+//     if (lowerCase) {
+//         charList = charList + "abcdefghijklmnopqrstuvwxyz";
+//     }
+
+//     if (charList == "") {
+//         alert("You need to select password criteria. Refresh page.");
+//         return;
+//     }
+
+//     //var pass is user password
+//     var pass = "";
